f4b683dfec62116dbb19cb836dc679b89ec1324c
Ann <annazorina@yandex.ru>
2024-06-02 23:38:59 +0500

    1.2. Входные данные в тесте [#297750]

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -22,34 +22,58 @@ class X2Test {
     }
 
     @Test
+<<<<<<< HEAD
     void whenA0B1C1X1Then0() {
+=======
+    void whenA0B1C1X1Then2() {
+>>>>>>> f600da4 (1.2. Входные данные в тесте [#297750])
         int a = 0;
         int b = 1;
         int c = 1;
         int x = 1;
+<<<<<<< HEAD
         int expected = 3;
+=======
+        int expected = 2;
+>>>>>>> f600da4 (1.2. Входные данные в тесте [#297750])
         int result = X2.calc(a, b, c, x);
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
+<<<<<<< HEAD
     void whenA1B1C0X1Then0() {
+=======
+    void whenA1B1C0X1Then2() {
+>>>>>>> f600da4 (1.2. Входные данные в тесте [#297750])
         int a = 1;
         int b = 1;
         int c = 0;
         int x = 1;
+<<<<<<< HEAD
         int expected = 3;
+=======
+        int expected = 2;
+>>>>>>> f600da4 (1.2. Входные данные в тесте [#297750])
         int result = X2.calc(a, b, c, x);
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
+<<<<<<< HEAD
     void whenA1B1C1X0Then0() {
+=======
+    void whenA1B1C1X0Then1() {
+>>>>>>> f600da4 (1.2. Входные данные в тесте [#297750])
         int a = 1;
         int b = 1;
         int c = 1;
         int x = 0;
+<<<<<<< HEAD
         int expected = 3;
+=======
+        int expected = 1;
+>>>>>>> f600da4 (1.2. Входные данные в тесте [#297750])
         int result = X2.calc(a, b, c, x);
         assertThat(result).isEqualTo(expected);
     }
