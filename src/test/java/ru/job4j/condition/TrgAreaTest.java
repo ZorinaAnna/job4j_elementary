091c78e23885354b11580330dee7beb842bdafed
Ann <annazorina@yandex.ru>
2024-06-10 22:33:35 +0500

    3. Тесты для определения расстояния между точками. [#159459]

---
+++ src/test/java/ru/job4j/condition/TrgAreaTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.withPrecision;
+
+class TrgAreaTest {
+
+    @Test
+    void whenP6K2Square2() {
+        double expected = 2;
+        int p = 6;
+        double k = 2;
+        double output = TrgArea.area(p, k, k);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenP9K3Square6() {
+        double expected = 6;
+        int p = 9;
+        double k = 3;
+        double output = TrgArea.area(p, k, k);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenP10K4Square8() {
+        double expected = 8;
+        int p = 10;
+        double k = 4;
+        double output = TrgArea.area(p, k, k);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+}
+}
