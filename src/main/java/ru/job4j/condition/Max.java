3042b2106c200a57a100145d59078a7cadea7aa8
Ann <annazorina@yandex.ru>
2024-07-01 23:11:36 +0500

    4.2. Тернарное сравнение [#189]

--- src/main/java/ru/job4j/condition/Max.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j.condition;
-
-public class Max {
-    public static String max(int left, int right) {
-        boolean condition = left > right;
-        return condition ? "Условие выполняется" : "Условие не выполняется";
-    }
-}
