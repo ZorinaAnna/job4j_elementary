b4d2d658ee61681bf46bd3240ae47bc3a182aaca
Ann <annazorina@yandex.ru>
2024-07-23 00:38:15 +0500

    4.5. Отладка программы в IDEA [#159464 #506055]

--- src/main/java/ru/job4j/condition/ThreeMax.java
+++ src/main/java/ru/job4j/condition/ThreeMax.java
@@ -6,7 +6,7 @@ public class ThreeMax {
         if (first < second && second < third) {
             result = third;
         }
-        if (second >= first && third > third) {
+        if (second >= first && second >= third) {
             result = second;
         }
         return result;
