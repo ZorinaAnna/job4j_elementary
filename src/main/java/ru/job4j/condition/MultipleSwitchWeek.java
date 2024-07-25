97523d9a08fc9dea69a33a082d9a35eac15dd568
Ann <annazorina@yandex.ru>
2024-07-26 01:29:27 +0500

    4.6.2. Switch - matcher [#334071]

---
+++ src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+public class MultipleSwitchWeek {
+    public static int numberOfDay(String name) {
+        return switch (name) {
+            case "Понедельник" -> 1;
+            case "Вторник" -> 2;
+            case "Среда" -> 3;
+            case "Четверг" -> 4;
+            case "Пятница" -> 5;
+            case "Суббота" -> 6;
+            case "Воскресенье" -> 7;
+            default -> -1;
+        };
+    }
+}
