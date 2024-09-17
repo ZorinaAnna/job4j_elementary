fa14afbd77c87c1a9988efbfae958c35d078d5c3
Ann <annazorina@yandex.ru>
2024-09-17 07:27:28 +0500

    5.2. Создать программу, вычисляющую факториал

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,6 +1,9 @@
 package ru.job4j.calculator;
 
 public class Calculator {
+    public static int plus(int a, int b) {
+        return a + b;
+    }
 
     public static void main(String[] args) {
         int one = 1;
@@ -8,13 +11,22 @@ public class Calculator {
         int six = 6;
         int four = 4;
         int five = 5;
+
         int onePlusTwo = one + two;
         System.out.println(onePlusTwo);
+
         int sixDivTwo = six / two;
         System.out.println(sixDivTwo);
+
         int fiveMinusTwo = five - two;
         System.out.println(fiveMinusTwo);
-        int fourTimeTwo = four * two;
-        System.out.println(fourTimeTwo);
+
+        int fourTimesTwo = four * two;
+        System.out.println(fourTimesTwo);
+
+        // Вызовы метода plus
+        System.out.println(plus(100, 500));
+        System.out.println(plus(4, 2));
+        System.out.println(plus(3, 5));
     }
 }
